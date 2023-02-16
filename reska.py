5b4e847e8cdcec78a9cbf8a06f98136815a847c7
Ado76 <119083581+Ador077@users.noreply.github.com>
2023-02-16 13:48:04 +0300

    Update reska.py

--- reska.py
+++ reska.py
@@ -43,37 +43,7 @@ def xox(z):
 		sys.stdout.flush()
 		time.sleep(0.01)
             
-P = '\x1b[1;97m'
-M = '\x1b[1;91m'
-H = '\x1b[1;92m'
-K = '\x1b[1;93m'
-B = '\x1b[1;94m'
-U = '\x1b[1;95m' 
-O = '\x1b[1;96m'
-N = '\x1b[0m'    
-Z = "\033[1;30m"
-sir = '\033[41m\x1b[1;97m'
-x = '\33[m' # DEFAULT
-m = '\x1b[1;91m' #RED +
-k = '\033[93m' # KUNING +
-xr = '\x1b[1;92m' # HIJAU +
-hh = '\033[32m' # HIJAU -
-u = '\033[95m' # UNGU
-kk = '\033[33m' # KUNING -
-b = '\33[1;96m' # BIRU -
-p = '\x1b[0;34m' # BIRU +
-asu = random.choice([m,k,xr,u,b])
-my_color = [
- P, M, H, K, B, U, O, N]
-warna = random.choice(my_color)
-now = datetime.now()
-dt_string = now.strftime("%H:%M")
-current = datetime.now()
-ta = current.year
-bu = current.month
-ha = current.day
-today = date.today()
-os.system('xdg-open https://facebook.com/ft.alvi.18')
+
 logo =("""\033[0;92m
 \033[1;34m   ____  _   __      ______________  ______
 \033[1;32m  / __ \/ | / /     / ____/  _/ __ \/ ____/
